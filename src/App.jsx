d0669fc7590487d6a815b6bf2c2d98becb11b96a
thejyotipatel <jyotipatelfyit77100@gmail.com>
2023-09-11 21:38:02 +0530

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
